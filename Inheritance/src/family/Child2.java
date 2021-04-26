62fd5e82dbad484a0d6ca03c0a37d2bbdd0a43f6
USER <darkcity93@naver.com>
2021-04-26 16:25:05 +0900

    [Inheritance] 상속

--- Inheritance/src/family/Child2.java
+++ Inheritance/src/family/Child2.java
@@ -1,16 +1,10 @@
 /*
  * 부모가 가지고 있는 필드(money)를 자식이 다시 정의 할 수 있다.
- * 부보의 메소드를 통해서 부모에 정의 된 필드(money)에 접근하게 되면
+ * 부모의 메소드를 통해서 부모와 자식에 정의 된 필드(money)에 접근하게 되면
  * 자신의 필드(money)가 아닌 부모의 필드(money) 값을 참조하게 된다.
  */
-
 package family;
 
 public class Child2 extends Parent {
 	int money = 900;
-	/*
-	void print(String title) {
-		System.out.printf("Child2 : [%s] money=(%d)\n", title, this.money);
-	}
-	*/
 }
