36fa9e8adb5eafc4be219ebce461acb6de4daad9
USER <darkcity93@naver.com>
2021-04-26 14:31:56 +0900

    [Inheritance] phone package : add file, CellPhone.java, DmpCellPhone.java

---
+++ Inheritance/src/phone/DmbCellPhoneExample.java
@@ -0,0 +1,25 @@
+package phone;
+
+public class DmbCellPhoneExample {
+
+	public static void main(String[] args) {
+		DmbCellPhone dmbCellPhone = new DmbCellPhone("자바폰", "검정", 10);
+	
+		
+		// 상속받은 부모의 메소드
+		dmbCellPhone.powerOn();
+		dmbCellPhone.bell();
+		dmbCellPhone.sendVoice("안녕하세요");
+		dmbCellPhone.receiveVoice("반갑습니다");
+		
+		
+		// 자신의 메소드
+		dmbCellPhone.turnOnDmb();
+		dmbCellPhone.changeChannelDmb(11);
+		dmbCellPhone.turnOffDmb();
+		
+		System.out.println("DmbCellPhone.channer=" + dmbCellPhone.channel);
+		
+	}
+
+}
